db63954fccd9b32afe76a40c82fa904045f0e54d
channahuda <huda.mumtazchanna@gmail.com>
2022-11-19 10:31:54 +0500

    port error solved

--- frontend/src/actions/ProductAction.js
+++ frontend/src/actions/ProductAction.js
@@ -13,7 +13,7 @@ import {
 export const getProduct = () => async (dispatch) => {
     try {
         dispatch({ type: ALL_PRODUCT_REQUEST })
-        const { data } = await axios.get("/product/getProducts")
+        const { data } = await axios.get("http://localhost:3000/products/getProducts")
         dispatch({
             type: ALL_PRODUCT_SUCCESS,
             payload: data
